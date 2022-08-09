3a15bf288d2d5a60bbaddc1419ad0438b9aa5f84
Илья Поляков <valeriadolgovskaa@MacBook-Air-Valeria.local>
2022-08-09 17:38:03 +0300

    first publication

---
+++ src/components/CartFooter /index.jsx
@@ -0,0 +1,16 @@
+import formatPrice from "../../utils/priceFormatter";
+import "./style.scss";
+
+const CartFooter = ({total}) => {
+    const {count, price} = total;
+    return (
+        <footer className="cart-footer">
+            <div className="cart-footer__count">{count} ед.</div>
+            <div className="cart-footer__price">
+                {formatPrice(price)} руб.
+            </div>
+        </footer>
+    );
+}
+ 
+export default CartFooter;
---
+++ src/components/Product/index.jsx
@@ -0,0 +1,26 @@
+import ButtonDelete from "../ButtonDelete";
+import Count from "../Count";
+import formatPrice from "../../utils/priceFormatter";
+import "./style.scss";
+
+const Product = ({product, deleteProduct, increase, changeValue, decrease}) => {
+    const { img, title, priceTotal, count, id} = product;
+
+    return (
+        <section className="product">
+            <div className="product__img"><img src={`./img/products/${img}`} alt={title}/></div>
+            <div className="product__title">{title}</div>
+            <div className="product__count">
+                <Count count={count} increase={increase} decrease={decrease} changeValue={changeValue} id={id}/>
+            </div>
+            <div className="product__price">
+                {formatPrice(priceTotal)} руб.
+            </div>
+            <div className="product__controls">
+                <ButtonDelete deleteProduct={deleteProduct} id={id}/>
+            </div>
+        </section>
+    );
+};
+ 
+export default Product;
